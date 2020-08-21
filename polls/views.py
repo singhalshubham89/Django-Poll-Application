7027855b7b2fe0bf01f43beb7538a408d565d820
Shubham Singhal <singhalshubham89@gmail.com>
2020-08-21 22:27:05 +0530

    Testing Django
    https://docs.djangoproject.com/en/3.1/intro/tutorial05/

--- polls/views.py
+++ polls/views.py
@@ -6,6 +6,7 @@ from .models import Choice, Question
 from django.shortcuts import render
 from django.http import Http404
 from django.views import generic
+from django.utils import timezone
 # Create your views here.
 
 
@@ -14,7 +15,9 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
@@ -27,6 +30,9 @@ class DetailView(generic.DetailView):
     # return render(request, 'polls/detail.html', {'question': question})
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DeleteView):
